68d8b82e7edb73cb2a8de853a1cb2f0b94fe6496
TJ <2587108+spectrshiv@users.noreply.github.com>
2022-05-22 23:55:05 -0500

    completed and refactored client and separated endpoints from main class. started working in validation logic.

---
+++ weather_gov/endpoints.py
@@ -0,0 +1,210 @@
+from weather_gov import validations
+
+
+class Base_Endpoint:
+    def __init__(self, parent) -> None:
+        self.parent = parent
+        self._wfo = validations.VALID_WFO
+        self._region = validations.VALID_REGION
+        self._region_type = validations.VALID_REGION_TYPE
+        self._urgency = validations.VALID_URGENCY
+        self._severity = validations.VALID_SEVERITY
+        self._certainty = validations.VALID_CERTAINTY
+        self._zone_type = validations.VALID_ZONE_TYPES
+
+
+class Alerts(Base_Endpoint):
+    def __call__(self, alertID: str=None, **params):
+
+        if alertID:
+            return self.parent.get(f"alerts/{alertID}", params)
+        return self.parent.get("alerts", params)
+            
+    def active(self, **params):
+
+        return self.parent.get("alerts/active", params)
+    
+    def active_count(self, **params):
+        return self.parent.get("alerts/active/count", params)
+    
+    def active_zone(self, zoneID: str, **params):
+        return self.parent.get(f"alerts/active/zone/{zoneID}", params)
+
+    def active_area(self, area: str, **params):
+        return self.parent.get(f"alerts/active/area/{area}", params)
+    
+    def active_region(self, region: str, **params):
+        
+        return self.parent.get(f"alerts/active/region/{region}", params)
+    
+    def types(self, **params):
+        return self.parent.get("alerts/types", params)
+
+
+class Gridpoints(Base_Endpoint):
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+        self._features = validations.VALID_GRIDPOINT_FORCAST_FEATURES
+    
+    def __call__(self, wfo:str, x:int, y:int, **params):
+        if wfo not in self._wfo:
+            raise ValueError(f"{wfo} is not a valid WFO")
+        self.parent.get(f"gridpoints/{wfo}/{x}/{y}", params)
+        
+    def forcast(self, wfo:str, x:int, y:int, features:list=None, **params):
+        for feature in features:
+            if feature not in self._features:
+                raise ValueError(f"{feature} is not a valid feature")
+        if wfo not in self._wfo:
+            raise ValueError(f"{wfo} is not a valid WFO")
+        return self.parent.get(f"gridpoints/{wfo}/{x}/{y}/forecast", params, feature_flags=features)
+    
+    def forecast_hourly(self, wfo:str, x:int, y:int, features:list=None, **params):
+        for feature in features:
+            if feature not in self._features:
+                raise ValueError(f"{feature} is not a valid feature")
+        if wfo not in self._wfo:
+            raise ValueError(f"{wfo} is not a valid WFO")
+        return self.parent.get(f"gridpoints/{wfo}/{x}/{y}/forecast/hourly", params, feature_flags=features)
+
+    def stations(self, wfo:str, x:int, y:int, **params):
+        if wfo not in self._wfo:
+            raise ValueError(f"{wfo} is not a valid WFO")
+        return self.parent.get(f"gridpoints/{wfo}/{x}/{y}/stations", params)
+
+
+class Stations:
+    def __init__(self, parent) -> None:
+        self.parent = parent
+        
+    def __call__(self, **params):
+        return self.parent.get(f"stations", params)
+    
+    def id(self, stationID: str, **params):
+        return self.parent.get(f"stations/{stationID}", params)
+    
+    def id_observations(self, stationID: str, **params):
+        return self.parent.get(f"stations/{stationID}/observations", params)
+    
+    def id_observations_latest(self, stationID: str, **params):
+        return self.parent.get(f"stations/{stationID}/observations/latest", params)
+    
+    def id_observations_time(self, stationID: str, time:str,  **params):
+        return self.parent.get(f"stations/{stationID}/observations/{time}", params)
+    
+class Offices(Base_Endpoint):
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+        
+    def __call__(self, **params):
+        return self.parent.get(f"offices", params)
+    
+    def id(self, officeID: str, **params):
+        if officeID not in self._wfo:
+            raise ValueError(f"{officeID} is not a valid WFO")
+        return self.parent.get(f"offices/{officeID}", params)
+    
+    def headlines(self, officeID: str, **params):
+        if officeID not in self._wfo:
+            raise ValueError(f"{officeID} is not a valid WFO")
+        return self.parent.get(f"offices/{officeID}/headlines", params)
+
+    def headline_id(self, officeID: str, headlineID: str, **params):
+        if officeID not in self._wfo:
+            raise ValueError(f"{officeID} is not a valid WFO")
+        return self.parent.get(f"offices/{officeID}/headlines/{headlineID}", params)
+
+
+class Points:
+    def __init__(self, parent) -> None:
+        self.parent = parent
+
+    def __call__(self):
+        raise NotImplementedError("Points() method is not implemented")
+
+    def id(self, pointID: str, **params):
+        return self.parent.get(f"points/{pointID}", params)
+        
+        
+class Radar(Base_Endpoint):
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+
+    def __call__(self):
+        raise NotImplementedError("Radar() method is not implemented")
+    
+    def servers(self, **params):
+        return self.parent.get("radar/servers", params)
+
+    def server_id(self, serverID: str, **params):
+        return self.parent.get(f"radar/servers/{serverID}", params)
+    
+    def stations(self, **params):
+        return self.parent.get("radar/stations", params)
+    
+    def station_id(self, stationID: str, **params):
+        return self.parent.get(f"radar/stations/{stationID}", params)
+    
+    def station_id_alarms(self, stationID: str, **params):
+        return self.parent.get(f"radar/stations/{stationID}/alarms", params)
+
+    def queues(self, host:str, **params):
+        return self.parent.get(f"radar/queues/{host}", params)
+    
+
+class Products:
+    def __init__(self, parent) -> None:
+        self.parent = parent
+        
+    def __call__(self, **params):
+        return self.parent.get("products", params)
+    
+    def locations(self, **params):
+        return self.parent.get("products/locations", params)
+    
+    def types(self, **params):
+        return self.parent.get("products/types", params)
+    
+    def id(self, productID: str, **params):
+        return self.parent.get(f"products/{productID}", params)
+    
+    def type_id(self, typeID: str, **params):
+        return self.parent.get(f"products/types/{typeID}", params)
+    
+    def type_id_locations(self, typeID: str, **params):
+        return self.parent.get(f"products/types/{typeID}/locations", params)
+    
+    def location_id_types(self, locationID: str, **params):
+        return self.parent.get(f"products/locations/{locationID}/types", params)
+    
+    def type_id_location_id(self, typeID: str, locationID: str, **params):
+        return self.parent.get(f"products/types/{typeID}/locations/{locationID}", params)
+    
+    
+class Zones(Base_Endpoint):
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+        
+    def __call__(self, **params):
+        self.parent.get("zones", params)
+        
+    def type(self, zoneType: str, **params):
+        if zoneType not in self._zone_types:
+            raise ValueError(f"{zoneType} is not a valid zone type")
+        return self.parent.get(f"zones/types/{zoneType}", params)
+
+    def type_zone_id(self, zoneType: str, zoneID: str, **params):
+        if zoneType not in self._zone_types:
+            raise ValueError(f"{zoneType} is not a valid zone type")
+        return self.parent.get(f"zones/types/{zoneType}/{zoneID}", params)
+    
+    def type_zone_id_forecast(self, zoneType: str, zoneID: str, **params):
+        if zoneType not in self._zone_types:
+            raise ValueError(f"{zoneType} is not a valid zone type")
+        return self.parent.get(f"zones/types/{zoneType}/{zoneID}/forecast", params)
+
+    def forecast_id_observations(self, zoneID: str, **params):
+        return self.parent.get(f"zones/forecasts/{zoneID}/observations", params)
+    
+    def forecast_id_stations(self, zoneID: str, **params):
+        return self.parent.get(f"zones/forecasts/{zoneID}/stations", params)
---
+++ weather_gov/validations.py
@@ -0,0 +1,104 @@
+from msilib.schema import Error
+
+
+VALID_WFO = ["AKQ", "ALY", "BGM", "BOX", "BTV", "BUF", "CAE", 
+            "CAR", "CHS", "CLE", "CTP", "GSP", "GYX", "ILM", 
+            "ILN", "LWX", "MHX", "OKX", "PBZ", "PHI", "RAH", 
+            "RLX", "RNK", "ABQ", "AMA", "BMX", "BRO", "CRP", 
+            "EPZ", "EWX", "FFC", "FWD", "HGX", "HUN", "JAN", 
+            "JAX", "KEY", "LCH", "LIX", "LUB", "LZK", "MAF", 
+            "MEG", "MFL", "MLB", "MOB", "MRX", "OHX", "OUN", 
+            "SHV", "SJT", "SJU", "TAE", "TBW", "TSA", "ABR", 
+            "APX", "ARX", "BIS", "BOU", "CYS", "DDC", "DLH", 
+            "DMX", "DTX", "DVN", "EAX", "FGF", "FSD", "GID", 
+            "GJT", "GLD", "GRB", "GRR", "ICT", "ILX", "IND", 
+            "IWX", "JKL", "LBF", "LMK", "LOT", "LSX", "MKX", 
+            "MPX", "MQT", "OAX", "PAH", "PUB", "RIW", "SGF", 
+            "TOP", "UNR", "BOI", "BYZ", "EKA", "FGZ", "GGW", 
+            "HNX", "LKN", "LOX", "MFR", "MSO", "MTR", "OTX", 
+            "PDT", "PIH", "PQR", "PSR", "REV", "SEW", "SGX", 
+            "SLC", "STO", "TFX", "TWC", "VEF", "AER", "AFC", 
+            "AFG", "AJK", "ALU", "GUM", "HPA", "HFO", "PPG", 
+            "STU", "NH1", "NH2", "ONA", "ONP"] 
+
+# States
+VALID_STATE = ["AL", "AK", "AS", "AR", "AZ", "CA", "CO", "CT", 
+                   "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", 
+                   "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", 
+                   "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", 
+                   "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", 
+                   "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", 
+                   "VT", "VI", "VA", "WA", "WV", "WI", "WY"]
+
+# Marine area codes (NWS Directive 10-302)
+VALID_MARINE_AREA_CODE = ["AM", "AN", "GM", "LC", "LE", "LH", "LM", 
+                           "LO","LS", "PH", "PK", "PM", "PS", "PZ", "SL"]
+
+VALID_MARINE_REGION_CODE = ["AL", "AT", "GL", "GM", "PA", "PI"]
+
+VALID_LAND_REGION_CODE = ["AR", "CR", "ER", "PR", "SR", "WR"]
+
+VALID_AREA_CODE = VALID_STATE + VALID_MARINE_AREA_CODE
+
+VALID_REGION = ["AL", "AT", "GL", "GM", "PA", "PI"]
+
+VALID_REGION_TYPE = ["land", "marine"]
+
+VALID_URGENCY = ["Immediate", "Expected", "Future", "Past", "Unknown"]
+
+VALID_SEVERITY = ["Extreme", "Severe", "Moderate", "Minor", "Unknown"]
+
+VALID_CERTAINTY = ["Observed", "Likely", "Possible", "Unlikely", "Unknown"]
+
+VALID_UNITS = ["us","si"]
+
+VALID_GRIDPOINT_FORCAST_FEATURES = ["forecast_temperature_qv", "forecast_wind_speed_qv"]
+
+VALID_ALERT_STATUS = ["Actual", "Exercise", "System", "Test", "Draft"]
+
+VALID_ALERT_MESSAGE_TYPE = ["Alert", "Update", "Cancel", "Ack", "Error"]
+
+VALID_ALERT_CATEGORY = ["Met", "Geo", "Safety", "Security", "Rescue", "Fire", "Health", "Env", "Transport", "Infra", "CBRNE", "Other"]
+
+VALID_ALERT_SEVERITY = VALID_SEVERITY
+
+VALID_ALERT_CERTAINTY = VALID_CERTAINTY
+
+VALID_ALERT_URGENCY = VALID_URGENCY
+
+VALID_ALERT_RESPONSE = ["Shelter", "Evacuate", "Prepare", "Execute", "Avoid", "Monitor", "Assess", "AllClear", "None"]
+
+VALID_ZONE_TYPE = [ "land", "marine", "forecast", "public", "coastal", "offshore", "fire", "county"]
+
+
+param_map = {
+    'default': {
+        'area': VALID_AREA_CODE,
+        'units': VALID_UNITS,},
+    'alerts': {
+        'MessageType': VALID_ALERT_MESSAGE_TYPE,
+        'category': VALID_ALERT_CATEGORY,
+        'severity': VALID_ALERT_SEVERITY,
+        'certainty': VALID_ALERT_CERTAINTY,
+        'urgency': VALID_ALERT_URGENCY,
+        'status': VALID_ALERT_STATUS,
+        'response': VALID_ALERT_RESPONSE,},
+    'zones': {
+        'type': VALID_ZONE_TYPE,},
+}
+
+
+def validate_params(params, type):
+    """
+    Validates the parameters passed to the API.
+    """        
+    
+    if type in param_map["default"].keys():
+        for param in params:
+            if param not in param_map["default"][type]:
+                raise Error("Invalid parameter: %s" % param)
+    
+    for param in params:
+        if param not in param_map[type].keys():
+            return False
+    return True
